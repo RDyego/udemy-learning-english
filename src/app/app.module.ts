87a9e06d86ad783251ed450c28805824c8d17960
RDyego <dyego@alu.ufc.br>
2017-08-21 11:18:01 -0700

    Adds component Top and Panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PanelComponent } from './panel/panel.component';
+import { TopComponent } from './top/top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PanelComponent,
+    TopComponent
   ],
   imports: [
     BrowserModule
