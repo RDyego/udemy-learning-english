d7edf2ec9c24587af075f1de113e56c40ce85a5e
RDyego <dyego@alu.ufc.br>
2017-08-21 11:23:10 -0700

    Finished component: Top

--- src/app/top/top.component.ts
+++ src/app/top/top.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopComponent implements OnInit {
 
+  public NavbarTitle: string = "Leaning English!"
+
   constructor() { }
 
   ngOnInit() {
